1eb4cb846e6cc38eedf1556450462fbdfd27a6e1
ZHHAYO <zhiyo2016@163.com>
2020-03-29 15:24:04 +0800

    update TestCalc()

--- testing/test_calc2.py
+++ testing/test_calc2.py
@@ -8,30 +8,39 @@ pytest
 '''
 import pytest
 import sys
+
 sys.path.append("..")
 from python.calc import Calc
 
+data1 = [(1, 2, 3),
+         (-1, -2, -3),
+         (0, 1, 1),
+         (0, -1, -1),
+         (0.1, 0.2, 0.3),
+         (999999, 1000000, 1999999)]
+
+data2 = [(1, 2, 0.5),
+          (-1, -2, 0.5),
+          (0, 1, 0),
+          (1, 0, 0),
+          (0.1, 0.2, 0.5)]
 
 class TestCalc():
     def setup(self):
         self.calc = Calc()
 
+    @pytest.mark.parametrize("a, b, result", data1)
+    def test_add(self, a, b, result):
+        assert self.calc.add(a, b) == result
+
+
+
+    @pytest.mark.parametrize("a, b, result", data2)
+    def test_div(self, a, b, result):
+        assert self.calc.div(a, b) == result
+
+
     def test_add_1(self):
-        assert self.calc.add(1, 2) == 3
-
-    def test_add_2(self):
-        assert self.calc.add(-1, -2) == -3
-
-    def test_add_3(self):
-        assert self.calc.add(1.1, 2.1) == 3.2
-
-    def test_params(self):
         data = (1, 2)
         assert self.calc.add_1(data) == 3
         assert self.calc.add(*data) == 3
-
-    def test_div_1(self):
-        assert self.calc.div(1, 2) == 0.5
-
-    def test_div_2(self):
-        assert self.calc.div(-1, 2) == -0.5
