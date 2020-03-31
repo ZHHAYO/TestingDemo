af4f5e23d464dc0317ba247ea856ca891b5a0654
ZHHAYO <zhiyo2016@163.com>
2020-03-31 22:08:37 +0800

    update test_calc.py

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -17,7 +17,7 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(3, self.calc.add(1, 2))
 
     def test_add_2(self):
-        self.assertEqual(0.03, self.calc.add(0.03, 0.02))
+        self.assertEqual(0.05, self.calc.add(0.03, 0.02))
 
 
 if __name__ == '__main__':
