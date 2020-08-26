597d8930cbc69136c6634d784ccb036250ce5ad5
zhiyo2016@163.com <zhiyo2016@163.com>
2020-08-26 20:06:51 +0800

    delete testing folder

--- testing/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-#-*-coding:utf-8-*-
-# @Time:    2020/3/29 16:28
-# @Author:  haiyong
-# @File:    __init__.py
--- testing/testing/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-#-*-coding:utf-8-*-
-# @Time:    2020/4/1 22:41
-# @Author:  haiyong
-# @File:    __init__.py.py
